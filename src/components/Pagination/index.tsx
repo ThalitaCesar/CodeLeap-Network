9620c55016cdb362d51a892ecdd0c575e44915a6
ThalitaCesar <thalita.walleska@gmail.com>
2023-04-28 18:14:30 -0300

    beautiful

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,39 +1,35 @@
 import React from "react";
-import { Button, PaginationContainer } from "./styled";
+import {Button, PaginationContainer} from "./styled";
 
 interface PaginationProps {
-    currentPage: number;
-    totalPages: number;
-    onChange: (page: number) => void;
-  }
-  
-  export const Pagination: React.FC<PaginationProps> = ({
-    currentPage,
-    totalPages,
-    onChange,
-  }) => {
-    const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
-  
+    currentPage : number;
+    totalPages : number;
+    onChange : (page : number) => void;
+}
+
+export const Pagination : React.FC < PaginationProps > = ({currentPage, totalPages, onChange}) => {
+    const pages = Array.from({
+        length: totalPages
+    }, (_, i) => i + 1);
+
     return (
-      <PaginationContainer>
-        <Button onClick={() => onChange(currentPage - 1)} disabled={currentPage === 1}>
-          {"<"}
-        </Button>
-        {pages.map((page) => (
-          <Button
-            key={page}
-            onClick={() => onChange(page)}
-            disabled={page === currentPage}
-          >
-            {page}
-          </Button>
-        ))}
-        <Button
-          onClick={() => onChange(currentPage + 1)}
-          disabled={currentPage === totalPages}
-        >
-          {">"}
-        </Button>
-      </PaginationContainer>
+        <PaginationContainer>
+            <Button onClick={() => onChange(currentPage - 1)} disabled={currentPage === 1}>
+                {"<"}
+            </Button>
+            {pages.map((page) => (
+                <Button
+                    key={page}
+                    onClick={() => onChange(page)}
+                    disabled={page === currentPage}>
+                    {page}
+                </Button>
+            ))}
+            <Button
+                onClick={() => onChange(currentPage + 1)}
+                disabled={currentPage === totalPages}>
+                {">"}
+            </Button>
+        </PaginationContainer>
     );
-  };
+};
--- src/components/PostsTable/index.tsx
+++ src/components/PostsTable/index.tsx
@@ -1,93 +1,106 @@
-import { Pencil, Trash } from "phosphor-react";
-import { usePosts } from "../../context/ApiContext/usePosts";
-import { Container, Content, Icons, Title } from "./styles";
-import { useState, useEffect } from "react";
-import { EditPostModal } from "../EditPostModal";
-import { DeletePostModal } from "../DeletePostModal";
-import { Post } from '../../services/api';
-import { Pagination } from "../Pagination";
+import {Pencil, Trash} from "phosphor-react";
+import {usePosts} from "../../context/ApiContext/usePosts";
+import {Container, Content, Icons, Title} from "./styles";
+import {useState, useEffect} from "react";
+import {EditPostModal} from "../EditPostModal";
+import {DeletePostModal} from "../DeletePostModal";
+import {Post} from '../../services/api';
+import {Pagination} from "../Pagination";
 
 interface PostsTableProps {
-  isOpen?: boolean;
+    isOpen?: boolean;
 }
 
-export function PostsTable({ isOpen = true }: PostsTableProps) {
-  const [isEditPostModalOpen, setIsEditPostModalOpen] = useState(false);
-  const [isDeletePostModalOpen, setIsDeletePostModalOpen] = useState(false);
-  const [postSelected, setPostSelected] = useState<Post | undefined>();
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const POSTS_PER_PAGE = 10;
+export function PostsTable({
+    isOpen = true
+} : PostsTableProps) {
+    const [isEditPostModalOpen,
+        setIsEditPostModalOpen] = useState(false);
+    const [isDeletePostModalOpen,
+        setIsDeletePostModalOpen] = useState(false);
+    const [postSelected,
+        setPostSelected] = useState < Post | undefined > ();
+    const [posts,
+        setPosts] = useState < Post[] > ([]);
+    const [currentPage,
+        setCurrentPage] = useState(1);
+    const POSTS_PER_PAGE = 10;
 
-  const {getAllPosts} = usePosts()
+    const {getAllPosts} = usePosts()
 
-  useEffect(() => {
-    async function fetchAllPosts() {
-      const allPosts = await getAllPosts();
-      setPosts(allPosts);
+    useEffect(() => {
+        async function fetchAllPosts() {
+            const allPosts = await getAllPosts();
+            setPosts(allPosts);
+        }
+        fetchAllPosts();
+    }, [posts]);
+
+    function handleOpenEditPostModal(post : Post) {
+        setPostSelected(post);
+        setIsEditPostModalOpen(true);
     }
-    fetchAllPosts();
-  }, [posts]);
 
-  function handleOpenEditPostModal(post: Post) {
-    setPostSelected(post);
-    setIsEditPostModalOpen(true);
-  }
+    function handleCloseEditPostModal() {
+        setIsEditPostModalOpen(false);
+    }
 
-  function handleCloseEditPostModal() {
-    setIsEditPostModalOpen(false);
-  }
+    function handleOpenDeletePostModal(post : Post) {
+        setPostSelected(post);
+        setIsDeletePostModalOpen(true);
+    }
 
-  function handleOpenDeletePostModal(post: Post) {
-    setPostSelected(post);
-    setIsDeletePostModalOpen(true);
-  }
-
-  function handleCloseDeletePostModal() {
-    setIsDeletePostModalOpen(false);
-  }
+    function handleCloseDeletePostModal() {
+        setIsDeletePostModalOpen(false);
+    }
 
     const getCurrentPosts = () => {
-      const startIndex = (currentPage - 1) * POSTS_PER_PAGE;
-      const endIndex = startIndex + POSTS_PER_PAGE;
-      return posts.slice(startIndex, endIndex);
+        const startIndex = (currentPage - 1) * POSTS_PER_PAGE;
+        const endIndex = startIndex + POSTS_PER_PAGE;
+        return posts.slice(startIndex, endIndex);
     };
 
-  const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE);
+    const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE);
 
-  return (
-    <>
-      {isOpen && getCurrentPosts().map(post => (
-        <Container key={post.id}>
-          <Title>
-            <p>{post.title}</p>
-            <Icons>
-              <Trash
-                style={{
-                  marginRight: "7px"
-                }}
-                onClick={() => handleOpenDeletePostModal(post)} />
-              <Pencil onClick={() => handleOpenEditPostModal(post)} />
-            </Icons>
-          </Title>
-          <Content>{post.content}</Content>
-        </Container>
-      ))}
-      <Pagination currentPage={currentPage} totalPages={totalPages} onChange={setCurrentPage} />
-
-
-      {isEditPostModalOpen && postSelected && (
-        <EditPostModal
-          Post={postSelected}
-          isOpen={isEditPostModalOpen}
-          onRequestClose={handleCloseEditPostModal} />
-      )}
-      {isDeletePostModalOpen && postSelected && (
-        <DeletePostModal
-          Post={postSelected}
-          isOpen={isDeletePostModalOpen}
-          onRequestClose={handleCloseDeletePostModal} />
-      )}
+    return (
+    <> 
+    {
+        isOpen && getCurrentPosts().map(post => (
+            <Container key={post.id}>
+                <Title>
+                    <p>{post.title}</p>
+                    <Icons>
+                        <Trash
+                            style={{
+                            marginRight: "7px"
+                        }}
+                            onClick={() => handleOpenDeletePostModal(post)}/>
+                        <Pencil onClick={() => handleOpenEditPostModal(post)}/>
+                    </Icons>
+                </Title>
+                <Content>{post.content}</Content>
+            </Container>
+        ))
+    } < Pagination currentPage = {
+        currentPage
+    }
+    totalPages = {
+        totalPages
+    }
+    onChange = {
+        setCurrentPage
+    } /> {
+        isEditPostModalOpen && postSelected && (<EditPostModal
+            Post={postSelected}
+            isOpen={isEditPostModalOpen}
+            onRequestClose={handleCloseEditPostModal}/>)
+    }
+    {
+        isDeletePostModalOpen && postSelected && (<DeletePostModal
+            Post={postSelected}
+            isOpen={isDeletePostModalOpen}
+            onRequestClose={handleCloseDeletePostModal}/>)
+    } 
     </>
   )
 }
